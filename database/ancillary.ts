19c7ff6dec5c21a2b5310c58a3070dccaea459f7
Janith Rathnayaka <janithr@xeptaon.com>
2024-11-14 10:44:47 +0530

    move to code ui

--- database/ancillary.ts
+++
@@ -1,6 +0,0 @@
-import ancillaryData from "./processed/ancillary";
-
-export const ancillary = ancillaryData;
-
-export type Pillar = keyof typeof ancillary.pillars;
-export type DigitalRightsPillar = keyof typeof ancillary.digitalRightPillarColorMap;
--- database/index.ts
+++
@@ -1,3 +0,0 @@
-import { Convert } from "./processed/db";
-import json from "./processed/db.json";
-export const db = Convert.toDB(JSON.stringify(json));
