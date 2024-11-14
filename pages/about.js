672f97f7af43d404d3f35750e282f2583b9d7adb
Janith Rathnayaka <janithr@xeptaon.com>
2024-11-14 10:24:46 +0530

    add static content

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,14 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { useState, useEffect } from "react";
+import dbData from "../database/processed/db.json";
 
 export default function Home() {
+  const digital_right_pillar_definitions = dbData.digital_right_pillar_definitions;
   const pillars = ["Freedom of Expression, Assembly and Association", "Right to Privacy", "Right to Equality and Non-Discrimination"];
   const [selectedPillar, setSelectedPillar] = useState(pillars[0]);
   const [stages, setStages] = useState([]);
+  const [stagesDemo, setStagesDemo] = useState([]);
 
   // Function to fetch all stages related to the selected pillar
   const digitalRightFetchStages = async (pillar) => {
@@ -19,10 +22,29 @@ export default function Home() {
     }
   };
 
+  const digitalRightFetchStagesDemo = async (pillar) => {
+    try {
+      if (!pillar) {
+        res.status(400).json({ message: "Pillar is required" });
+        return;
+      }
+  
+      // Use filter to return an array of all matching stages
+      const stages = digital_right_pillar_definitions
+      .filter(
+        (d) => d["Pillar"] === pillar
+      );
+      setStagesDemo(stages); // Store the fetched stages in the state
+    } catch (error) {
+      console.error("Error fetching stages:", error);
+    }
+  };
+
   // Call the function whenever the selected pillar changes
   useEffect(() => {
     if (selectedPillar) {
       digitalRightFetchStages(selectedPillar);
+      digitalRightFetchStagesDemo(selectedPillar)
     }
   }, [selectedPillar]);
 
@@ -61,6 +83,19 @@ export default function Home() {
           <ul>
             {stages.length > 0 ? (
               stages.map((stage, index) => (
+                <li key={index}>{JSON.stringify(stage)}</li>
+              ))
+            ) : (
+              <li>No stages found for the selected pillar.</li>
+            )}
+          </ul>
+        </div>
+
+        <div className={styles.stages}>
+          <h2>Initial Ones {selectedPillar}:</h2>
+          <ul>
+            {stagesDemo.length > 0 ? (
+              stagesDemo.map((stage, index) => (
                 <li key={index}>{JSON.stringify(stage)}</li>
               ))
             ) : (
