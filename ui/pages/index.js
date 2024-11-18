855a7daec45bc288e598383d6449744aafdddaf4
Janith Rathnayaka <janithr@xeptaon.com>
2024-11-18 13:10:18 +0530

    update

--- ui/pages/index.js
+++ ui/pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import logo  from "../public/vercel.svg"
 
 export default function Home() {
   return (
@@ -63,7 +64,7 @@ export default function Home() {
         >
           Powered by{" "}
           <span className={styles.logo}>
-            <Image src={'/vercel.svg'} alt="Vercel Logo" width={72} height={16} />
+            <Image src={logo} alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
